d6a198a8c7bfd63710dda0c6963c321a896f3b13
vallisu <syslik110@gmail.com>
2017-02-10 16:11:41 +0300

    Final version

--- array.py
+++ array.py
@@ -2,11 +2,20 @@
 # Необходимо получить результирующий массив, который будет равен первому массиву с исключёнными
 # значениями из второго массива.
 
+# 1st option - without using collections
 first_list = ['Alex', 'Dima', 'Kate', 'Galina', 'Ivan']
 second_list = ['Dima', 'Ivan', 'Kate']
+result_list = first_list.copy()
 
 for item in first_list:
     if item in second_list:
-        first_list.remove(item)
+        result_list.remove(item)
 
-print(first_list)
+print(result_list)
+
+
+# 2nd option - using collections
+first_list_coll = {"Alex", "Dima", "Kate", "Galina", "Ivan"}
+second_list_coll = {"Dima", "Ivan", "Kate"}
+
+print(first_list_coll - second_list_coll)
